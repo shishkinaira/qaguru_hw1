81042afed20374eb586b786a324d39824b1efb8e
ishishkina <shishkinaira@gmail.com>
2023-06-13 14:34:28 +0300

    change message for the example from the lesson

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -2,8 +2,8 @@ package guru.qa;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello, qa.guru!! Text added");
-        System.out.println("Hello, qa.guru!! Text edited for conflict");
-        что-то там для конфликта
+        if(args.length == 0){
+            System.out.println("Hello, qa.guru!");
+        }
     }
 }
