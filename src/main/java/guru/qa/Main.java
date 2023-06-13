6e8c77f9e617e042a31b7b1064b7b2a809bca0db
ishishkina <shishkinaira@gmail.com>
2023-06-13 14:39:27 +0300

    change message from the branch conflict2

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -3,7 +3,7 @@ package guru.qa;
 public class Main {
     public static void main(String[] args){
         if(args.length == 0){
-            System.out.println("Hello, qa.guru!");
+            System.out.println("Hello, qa.guru!!!");
         }
     }
 }
