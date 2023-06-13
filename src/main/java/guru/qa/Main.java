0e908ed9f8726a47ed2649c59138d73dcc0621b6
ishishkina <shishkinaira@gmail.com>
2023-06-13 13:48:58 +0300

    change message5

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -2,6 +2,8 @@ package guru.qa;
 
 public class Main {
     public static void main(String[] args){
+        System.out.println("Hello, qa.guru!! Text added");
         System.out.println("Hello, qa.guru!! Text edited for conflict");
+        что-то там для конфликта
     }
 }
